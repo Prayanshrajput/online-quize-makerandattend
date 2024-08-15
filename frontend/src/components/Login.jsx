7163e4cb157405e03d59ce8a52742c422219caec
Prayanshrajput <prayansh75@gmail.com>
2024-08-15 17:46:33 +0530

    singup problem

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -84,42 +84,6 @@ export const Login = () => {
     }
    }
 
-  // return (
-  //   <div className="flex flex-col h-screen w-screen justify-center items-center">
-  //       <div className="text-[40px] text-indigo-700 font-extrabold">Quiz_Maker</div>
-  //       <form className="flex flex-col justify-around  w-[500px] h-[350px] border border-black bg-gray-300" action="">
-           
-  //        <div className="flex flex-col gap-2">
-  //        <label className="text-[20px] font-bold  " htmlFor="">Enter your mail</label>
-  //        <input className=" flex h-[50px] border border-black font-bold" name="email" onChange={Inputhandler} type="email" placeholder="xyz@gmail.com" />
-  //        </div>
-
-  //          <div className="flex flex-col gap-2">
-  //          <label className="text-[20px] font-bold" htmlFor="">Enter your password</label>
-          
-  //          <div class="w-[100%] flex items-center border border-black font-bold  bg-white">
-  //           <input class="flex-grow h-[50px]  focus:outline-none " name='password' type={pass?"password":"text"} onChange={Inputhandler} placeholder="Enter your password"/>
-  //         {Showhandler && <div class="cursor-pointer p-2" onClick={passwordhandler}>{pass?"Show":"Hide"}</div>}
-  //           </div>
-  //          </div>
-
-  //           <div className="flex justify-center">
-  //           <button className="flex justify-center items-center w-[100px] h-[50px] bg-blue-600 text-[20px] font-extrabold opacity-70 hover:opacity-100 " onClick={submithandler}>Login</button>
-  //           </div>
-
-  //           <div className="flex  justify-center items-center text-blue-600">
-  //               <NavLink to="/forget"  className="hover:font-bold">
-  //               Forget Password  |
-  //               </NavLink>
-  //               <NavLink to="/singup"  className="hover:font-bold">
-  //               | Sing-Up
-  //               </NavLink>
-            
-  //           </div>
-  //       </form>
-  //       <ToastContainer/>
-  //   </div>
-  // )
 
 return (
   <div className="flex w-screen h-screen justify-center items-center">       
@@ -130,9 +94,10 @@ return (
         <input className="flex w-[528px] h-[56px] rounded-[8px] bg-[#EBEBEB] pr-[12px] pl-[12px]" type="email" name="email" onChange={Inputhandler} value={data.email} id="" placeholder="Your email" />
 
         <div className="flex w-[528px] h-[56px] rounded-[8px] bg-[#EBEBEB] ">
-        <input className="w-[90%] bg-[#EBEBEB] rounded-[8px] focus:outline-none   pr-[12px] pl-[12px] " name="password" onChange={Inputhandler} value={data.password} type={pass?"text":"password"} placeholder='Password' />
+        <input className="w-[90%] bg-[#EBEBEB] rounded-[8px] focus:outline-none   pr-[12px] pl-[12px] " name="password" onChange={Inputhandler} value={data.password} type={pass?"password":"text"} placeholder='Password' />
        <div className="w-[10%] flex justify-center items-center"> 
-        <img src='/show.png' className="w-[24px] h-[24px]" onClick={passwordhandler} alt=""/>
+        {/* <img src='/show.png' className="w-[24px] h-[24px]" onClick={passwordhandler} alt=""/> */}
+        {pass?<div onClick={passwordhandler}>Show</div >:<div onClick={passwordhandler}>Hide</div>}
        </div>
         </div>
 
--- frontend/src/components/Singup.jsx
+++ frontend/src/components/Singup.jsx
@@ -46,8 +46,8 @@ export const Singup = () => {
         if(data.username==""||data.email==""){
             toast.error("please full fill your entries")   
         }
-       else if(data.password!=data.conformpassword){
-            toast.error("Mach your password")
+        else if(data.password!=data.conformpassword){
+            toast.error("Password missmatch")  
         }
         else{
             postdata()
@@ -94,61 +94,28 @@ export const Singup = () => {
 
 
   return (
-    // <div className="flex flex-col h-screen w-screen justify-center items-center">
-    //     <div className="text-[40px] text-indigo-700 font-extrabold">Quiz_Maker</div>
-    //     <form className="flex flex-col justify-around  w-[500px] h-fit border border-black bg-gray-300" action="">
-           
-    //     <div className="flex flex-col gap-2">
-    //      <label className="text-[20px] font-bold  " htmlFor="">Enter your userName</label>
-    //      <input className=" flex h-[50px] border border-black font-bold" name='username' onChange={changehandler} type="text" placeholder="Enter your name" />
-    //      </div>
-
-    //      <div className="flex flex-col gap-2">
-    //      <label className="text-[20px] font-bold  " htmlFor="">Enter your mail</label>
-    //      <input className=" flex h-[50px] border border-black font-bold" name='email' onChange={changehandler} type="email" placeholder="xyz@gmail.com" />
-    //      </div>
-
-    //        <div className="flex flex-col gap-2">
-    //        <label className="text-[20px] font-bold" htmlFor="">Enter your password</label>
-          
-    //        <div class="w-[100%] flex items-center border border-black font-bold  bg-white">
-    //         <input class="flex-grow h-[50px]  focus:outline-none " name='password'  type={pass?"password":"text"} onChange={changehandler} placeholder="Enter your password"/>
-    //       {  Showhandler && <div class="cursor-pointer p-2" onClick={passwordhandler}>{pass?"Show":"Hide"}</div>}
-    //         </div>
-    //        </div>
-
-    //        <div className="flex flex-col gap-2">
-    //        <label className="text-[20px] font-bold" htmlFor="">Conform password</label>
-          
-    //        <div class="w-[100%] flex items-center border border-black font-bold  bg-white">
-    //         <input class="flex-grow h-[50px]  focus:outline-none " name='conformpassword' type={conformpass?"password":"text"} onChange={changehandler} placeholder="Enter your password"/>
-    //       {  conformshowhandler && <div class="cursor-pointer p-2" onClick={conformpasswordhandler}>{conformpass?"Show":"Hide"}</div>}
-    //         </div>
-    //        </div>
-
-    //         <div className="flex justify-center">
-    //         <button className="flex justify-center items-center w-[100px] h-[50px] bg-blue-600 text-[20px] font-extrabold opacity-70 hover:opacity-100 " onClick={submithandler}>Singup</button>
-    //         </div>
-
-    //         <div className="flex  justify-center items-center text-blue-600">
-    //             <NavLink to="/login"  className="hover:font-bold">
-    //              Login
-    //             </NavLink>
-    //         </div>
-    //     </form>
-    //     <ToastContainer />
-    // </div>
 
     <div className="flex w-screen h-screen justify-center items-center">       
-    <div className="flex flex-col gap-[32px] w-[648px] h-[556px]  rotate-[16px] border border-[#CECECE] p-[60px] bg-slate-50">
+    <div className="flex flex-col gap-[30px] w-[648px] h-[556px]  rotate-[16px] border border-[#CECECE] p-[60px] bg-slate-50">
     <div className="flex justify-center text-[40px] h-[58px] w-[528px] font-[600]">Welcome to<div className="text-[#4534AC]">&nbsp;Quiz_Master </div></div>
-   <div className="flex flex-col w-[528px] h-[290px] gap-[42px]">
-   <input className="flex w-[528px] h-[56px] rounded-[8px] border border-[#999999] bg-[#EBEBEB] pr-[12px] pl-[12px] " type="text" name="name" value={data.name} onChange={inputhandler} placeholder='Full name' />
+   <div className="flex flex-col w-[528px] h-[290px] gap-[15px]">
+   <input className="flex w-[528px] h-[56px] rounded-[8px] border border-[#999999] bg-[#EBEBEB] pr-[12px] pl-[12px] " type="text" name="username" value={data.username} onChange={inputhandler} placeholder='Full name' />
     <input className="flex w-[528px] h-[56px] rounded-[8px] bg-[#EBEBEB] pr-[12px] pl-[12px]" type="email"  id="" name="email" value={data.email} onChange={inputhandler} placeholder="Your email" />
    
     <div className="flex w-[528px] h-[56px] rounded-[8px] bg-[#EBEBEB] ">
-    <input className="w-[90%] bg-[#EBEBEB] rounded-[8px] focus:outline-none   pr-[12px] pl-[12px] " type={pass?"text":"password"} name='password' value={data.password} onChange={inputhandler} placeholder='Password' />
-   <div className="w-[10%] flex justify-center items-center"> <img src="/show.png" className="w-[24px] h-[24px]" onClick={passwordhandler} alt="" /></div>
+    <input className="w-[90%] bg-[#EBEBEB] rounded-[8px] focus:outline-none   pr-[12px] pl-[12px] " type={pass?"password":"text"} name='password' value={data.password} onChange={inputhandler} placeholder='Password' />
+   <div className="w-[10%] flex justify-center items-center"> 
+    {/* <img src="/show.png" className="w-[24px] h-[24px]" onClick={passwordhandler} alt="" /> */}
+    {pass?<div onClick={passwordhandler}>Show</div >:<div onClick={passwordhandler}>Hide</div>}
+    </div>
+    </div>
+
+    <div className="flex w-[528px] h-[56px] rounded-[8px] bg-[#EBEBEB] ">
+    <input className="w-[90%] bg-[#EBEBEB] rounded-[8px] focus:outline-none   pr-[12px] pl-[12px] " type={conformpass?"password":"text"} name='conformpassword' value={data.conformpassword} onChange={inputhandler} placeholder='Conform Password' />
+   <div className="w-[10%] flex justify-center items-center"> 
+    {/* <img src="/show.png" className="w-[24px] h-[24px]" onClick={passwordhandler} alt="" /> */}
+    {conformpass?<div onClick={conformpasswordhandler}>Show</div >:<div onClick={conformpasswordhandler}>Hide</div>}
+    </div>
     </div>
 
     <button onClick={submithandler} className="flex justify-center items-center w-[528px] h-[52px] text-[#FFFFFF] rounded-[8px] bg-gradient-to-t from-[#4B36CC] to-[#9C93D4]">Sing up</button>
